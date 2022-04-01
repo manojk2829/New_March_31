d3fd7ab7d113b432794a55206cdc7553a6b9bff0
Manoj Kushwaha <manojk2829@gmail.com>
2022-04-01 11:02:49 +0530

    Chandra

---
+++ src/testpackage/TestClass2.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+public class TestClass2 {
+	public static void test1() {
+		System.out.println("Chandra Bhai....");
+	}
+	
+	public static void main(String[] args) {
+		test1();
+	}
+
+}
