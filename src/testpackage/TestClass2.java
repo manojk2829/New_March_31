af78e705d3fac8cbbddeceb6cb9fd0734ebdbea3
Manoj Kushwaha <manojk2829@gmail.com>
2022-04-01 11:04:33 +0530

    Test Commit Test2

--- src/testpackage/TestClass2.java
+++ src/testpackage/TestClass2.java
@@ -2,7 +2,7 @@ package testpackage;
 
 public class TestClass2 {
 	public static void test1() {
-		System.out.println("Chandra Bhai....");
+		System.out.println("Chandra Bhai.... Text Added");
 	}
 	
 	public static void main(String[] args) {
